869d1f78671678baa32b74588d2b3a379a6574a8
hanx0r <crypto@hankus.org>
2026-06-28 09:42:59 +0200

    pngs

--- scripts/Publish.sh
+++ scripts/Publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "=============================="
-echo "        UPDATING OVERVIEW"
+echo "       UPDATING OVERVIEW"
 echo "=============================="
 
 sudo cp /home/hanx0r/Heimdall/html/*.html /var/cache/munin/www/
+sudo cp --update=none /home/hanx0r/Heimdall/html/*.png /var/cache/munin/www/static/
+
