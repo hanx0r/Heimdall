fd77d8806f66682d6ba0f096c5ac5852c1f39689
hanx0r <crypto@hankus.org>
2026-06-28 09:37:26 +0200

    init

---
+++ scripts/Publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "=============================="
+echo "        UPDATING OVERVIEW"
+echo "=============================="
+
+sudo cp /home/hanx0r/Heimdall/html/*.html /var/cache/munin/www/
